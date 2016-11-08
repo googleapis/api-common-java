25d63e409064bcc4d190727ebc103ea6883b7b85
Michael Bausor <michaelbausor@google.com>
2016-11-08 10:52:24 -0800

    Rename test class to TemplatedResourceNameTest

--- src/test/java/com/google/api/pathtemplate/TemplatedResourceNameTest.java
+++ src/test/java/com/google/api/pathtemplate/TemplatedResourceNameTest.java
@@ -42,7 +42,7 @@ import org.junit.runners.JUnit4;
  * templates, not much needs to be done here.
  */
 @RunWith(JUnit4.class)
-public class ResourceNameTest {
+public class TemplatedResourceNameTest {
 
   @Test
   public void resourceNameMethods() {
