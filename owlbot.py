087e41f750b25dbd5312fd985da7e53d3c49d6fd
Neenu1995 <neenushaji@google.com>
2021-04-21 15:05:40 -0400

    chore: remove staging

--- owlbot.py
+++ owlbot.py
@@ -15,10 +15,4 @@
 import synthtool as s
 from synthtool.languages import java
 
-
-for library in s.get_staging_dirs():
-    # put any special-case replacements here
-    s.move(library)
-
-s.remove_staging_dirs()
 java.common_templates()
