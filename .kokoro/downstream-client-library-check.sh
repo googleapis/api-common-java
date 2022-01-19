2c7976b4a110075a01216b22af7f68e92cb7ce13
Neenu1995 <neenushaji@google.com>
2022-01-19 13:29:05 -0500

    chore: generalize script

--- .kokoro/downstream-client-library-check.sh
+++ .kokoro/downstream-client-library-check.sh
@@ -20,46 +20,21 @@ set -eo pipefail
 # Display commands being run.
 set -x
 
-REPO=$1
+CORE_ARTIFACT=$1
+CLIENT_LIBRARY=$2
 ## Get the directory of the build script
 scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 ## cd to the parent directory, i.e. the root of the git repo
 cd ${scriptDir}/..
 
-COMMON_VERSION_TEXT=`gradle -q pV`
-COMMON_VERSION=$(echo "${COMMON_VERSION_TEXT}" | sed -e 's/\[.*\]$//g')
-echo $COMMON_VERSION
+CORE_VERSION_TEXT=`gradle -q pV`
+CORE_VERSION=$(echo "${CORE_VERSION_TEXT}" | sed -e 's/\[.*\]$//g')
+echo $CORE_VERSION
 
 # Publish api-common to local maven to make it available for downstream libraries
 ./gradlew build publishToMavenLocal
 
 # Round 1
-# Clone java-core
-git clone "https://github.com/googleapis/java-core.git" --depth=1
-cd java-core
-# replace version
-xmllint --shell <(cat pom.xml) << EOF
-setns x=http://maven.apache.org/POM/4.0.0
-cd .//x:artifactId[text()="api-common"]
-cd ../x:version
-set ${COMMON_VERSION}
-save pom.xml
-EOF
-# Make java-core artifacts available for 'mvn validate' at the bottom
-mvn install -DskipTests=true -Dmaven.javadoc.skip=true -Dgcloud.download.skip=true -B -V -q
-
-# Read the current version of this java-core in the POM. Example version: '0.116.1-alpha-SNAPSHOT'
-CORE_VERSION_POM=pom.xml
-# Namespace (xmlns) prevents xmllint from specifying tag names in XPath
-CORE_VERSION=`sed -e 's/xmlns=".*"//' ${CORE_VERSION_POM} | xmllint --xpath '/project/version/text()' -`
-
-if [ -z "${CORE_VERSION}" ]; then
-  echo "Version is not found in ${CORE_VERSION_POM}"
-  exit 1
-fi
-echo "Version: ${CORE_VERSION}"
-
-# Round 2
 # Check this java-core against HEAD of java-shared dependencies
 
 git clone "https://github.com/googleapis/java-shared-dependencies.git" --depth=1
@@ -68,7 +43,7 @@ pushd java-shared-dependencies/first-party-dependencies
 # replace version
 xmllint --shell <(cat pom.xml) << EOF
 setns x=http://maven.apache.org/POM/4.0.0
-cd .//x:artifactId[text()="google-cloud-core-bom"]
+cd .//x:artifactId[text()="${CORE_ARTIFACT}"]
 cd ../x:version
 set ${CORE_VERSION}
 save pom.xml
@@ -87,12 +62,12 @@ if [ -z "${SHARED_DEPS_VERSION}" ]; then
   exit 1
 fi
 
-# Round 3
-# Check this BOM against a few java client libraries
-git clone "https://github.com/googleapis/java-${REPO}.git" --depth=1
-pushd java-${REPO}
+# Round 2
+# Check this BOM against java client libraries
+git clone "https://github.com/googleapis/java-${CLIENT_LIBRARY}.git" --depth=1
+pushd java-${CLIENT_LIBRARY}
 
-if [[ $REPO == "bigtable" ]]; then
+if [[ $CLIENT_LIBRARY == "bigtable" ]]; then
   pushd google-cloud-bigtable-deps-bom
 fi
 
@@ -105,7 +80,7 @@ set ${SHARED_DEPS_VERSION}
 save pom.xml
 EOF
 
-if [[ $REPO == "bigtable" ]]; then
+if [[ $CLIENT_LIBRARY == "bigtable" ]]; then
   popd
 fi
 
